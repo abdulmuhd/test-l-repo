7df2add85b6c607d3525b14ed8679932b2af4b0e
abdulmuhd0@gmail.com <abdulmuhd0@gmail.com>
2022-12-24 16:55:18 +0100

    second

--- home.js
+++ home.js
@@ -1,21 +1,15 @@
 async function fetchUrl() {
 	const url = 'http://localhost:3000/blogs';
+	
 	try {
 		const response = await fetch(url);
 		const data = await response.json();
-		if (data.length > 0) {
-			data.forEach(d=>{
-				alert(d.author)
-			})
-		} else {
-			
-		}
+		
 		return data;
 	} catch (error) {
 		console.error(error);
 	}
 }
-
 const dataPromise = fetchUrl();
 dataPromise.then(function(data) {
 	
